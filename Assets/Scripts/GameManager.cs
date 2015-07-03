b538ea31884796f92c6a254a1adbd7d39b36b71d
nottvlike <nottvlike@gmail.com>
2015-07-03 17:44:42 +0800

    add project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	public List<Character> Characters = new List<Character>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+[System.Serializable]
+public class Character
+{
+	public string Name;
+	public Texture2D Icon;
+	public GameObject PlayerPrefab;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class PlayerController : MonoBehaviour {
+	public Animator Anim;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Anim.SetFloat ("Speed" , Input.GetAxis("Horizontal"));
+		Anim.SetBool ("IsRunning", Input.GetKeyDown (KeyCode.LeftShift));
+		Anim.SetFloat ("Direction" , Input.GetAxis("Vertical"));
+	}
+}
