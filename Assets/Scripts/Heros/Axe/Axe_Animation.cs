35414765394a113cc88fd6cb45f8dc493836b460
nottvlike <nottvlike@gmail.com>
2015-08-08 12:08:57 +0800

    add earthshaker,doom model,add attack speed

--- Assets/Scripts/Heros/Axe/Axe_Animation.cs
+++ Assets/Scripts/Heros/Axe/Axe_Animation.cs
@@ -1,24 +1,31 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class Axe_Animation : MonoBehaviour {
 	public GameObject parent;
-	public Animator anim;
+//	public Animator anim;
 	private bool isRunning = false;
 	private int isHit = 1;
 	private int state = 0;
 	private float RunningSpead = 2;
-
+//	[HideInInspector]
+	public float AttackSpeed = 0;
+	private float BasicBetween = 1.7f;
 	//int isHit = 1;
 	// Use this for initialization
 	void Start () {
-	
+		goIdle ();
+		AnimationEvent attackEvent = new AnimationEvent ();
+		attackEvent.time = animation ["mdldecompiler.qc_skeleton|attack_main"].length * 2 / 3;
+		attackEvent.functionName = "onAttackFinished";
+		animation ["mdldecompiler.qc_skeleton|attack_main"].clip.AddEvent ( attackEvent );
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isRunning) {
-			Vector3 vec = new Vector3( parent.transform.position.x , parent.transform.position.y , parent.transform.position.z + 2 * isHit * Time.deltaTime );
+			Vector3 vec = new Vector3( parent.transform.position.x , parent.transform.position.y , parent.transform.position.z + -2 * isHit * Time.deltaTime );
 			parent.transform.position = vec;
 		}
 	}
@@ -28,10 +35,12 @@ public class Axe_Animation : MonoBehaviour {
 		if (hit) {
 //			isHit = 0;
 			isRunning = false;
+			state = 0;
 		} else {
 //			isHit = 1;	
 			isRunning = true;
 		}
+		doByState ();
 	}
 
 	public void onSkillFinished()
@@ -40,11 +49,18 @@ public class Axe_Animation : MonoBehaviour {
 		doByState ();
 	}
 
+	void playAnim( string name )
+	{
+		animation.Play (name);	
+	}
+
 	void doByState()
 	{
 		switch (state) {
 		case 0:
-			anim.Play ("Idle");
+//			anim.Play ("Idle");
+//			playAnim("mdldecompiler.qc_skeleton|idle");
+			goIdle();
 			break;
 		case 1:
 			moveForward();
@@ -57,22 +73,50 @@ public class Axe_Animation : MonoBehaviour {
 		}
 	}
 
-	public void goAttack()
+	public void goIdle()
 	{
-		state = 2;
-		Debug.Log ("goAttack");
-		anim.Play ("attack_normal");
+		state = 0;
+//		anim.Play ("Idle");
+		animation ["mdldecompiler.qc_skeleton|idle"].wrapMode = WrapMode.Loop;
+		playAnim("mdldecompiler.qc_skeleton|idle");
+//		anim.animation["Idle"];
+//		transform.animation["Idle"];
 	}
 
+	public void goAttack()
+	{
+//		transform.animation ["Axe_Attack_Normal"].speed = 0.2f;
+//		AnimationClip mm = getMotionByName ("Axe_Attack_Normal") as AnimationClip;
+//		mm.frameRate = 3;
+//		Animation anim = new Animation ();
+//		anim.clip
+		state = 2;
+		Debug.Log ("goAttack");
+//		anim.Play ("attack_normal");
+		animation ["mdldecompiler.qc_skeleton|attack_main"].speed = animation ["mdldecompiler.qc_skeleton|attack_main"].length * (1 + AttackSpeed / 100) / BasicBetween;
+//		animation ["mdldecompiler.qc_skeleton|attack_main"].clip.AddEvent( new AnimationEvent())
+		playAnim("mdldecompiler.qc_skeleton|attack_main");
+//		Debug.Log ( anim.GetClip("attack_normal").name);
+	}
+
+	public void onAttackFinished()
+	{
+//		goIdle ();
+		Debug.Log ("Axe : onAttackFinished");
+//		parent.GetComponent<Axe> ().onAttackFinished ();
+//		yield return new WaitForSeconds (1);
+	}
 	public void goDead ()
 	{
-		anim.Play ("Death");
+//		anim.Play ("Death");
+		playAnim("mdldecompiler.qc_skeleton|death");
 	}
 
 	public void doSkill( int skillid)
 	{
 		if (skillid == 3) {
-			anim.Play ("attack_skill_dazhao");			
+//			anim.Play ("attack_skill_dazhao");
+			playAnim("mdldecompiler.qc_skeleton|culling_blade");
 		}
 //		anim.SetInteger ("Skill" , skillid );
 	}
@@ -84,12 +128,14 @@ public class Axe_Animation : MonoBehaviour {
 	public void moveForward()
 	{
 		state = 1;
-		anim.Play ("run");
+//		anim.Play ("run");
+		animation ["mdldecompiler.qc_skeleton|run"].wrapMode = WrapMode.Loop;
+//		animation ["mdldecompiler.qc_skeleton|run"].clip.a
+		playAnim ("mdldecompiler.qc_skeleton|run");
 //		anim.SetBool("Run" , true );
 		isRunning = true;
 //		RunningSpead = 1;
 //		isHit = 1;
 	}
-
 //	public void 
 }
--- Assets/Scripts/Heros/EarthShaker/EarthShaker.cs
+++ Assets/Scripts/Heros/EarthShaker/EarthShaker.cs
@@ -1,29 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Axe : MonoBehaviour {
+public class EarthShaker : MonoBehaviour {
 	public GameObject _anim;
 	public GameObject _coll;
-	private Axe_Animation axe_animation;
-	private Axe_Collider axe_collider;
+	private EarthShaker_Animation hero_animation;
+	private EarthShaker_Collider hero_collider;
 	private Collision hitCollider = null;
 	private int state = 0;
-
+	
 	void Awake()
 	{
-		axe_animation = _anim.GetComponent<Axe_Animation> ();
-		axe_collider = _coll.GetComponent<Axe_Collider> ();
+		hero_animation = _anim.GetComponent<EarthShaker_Animation> ();
+		hero_collider = _coll.GetComponent<EarthShaker_Collider> ();
 	}
-
 	// Use this for initialization
 	void Start () {
 		StartCoroutine ("PlayerState");
 	}
 	
+	// Update is called once per frame
 	void Update () {
-
+		
 	}
-
+	
 	public void onColliderEnter( Collision ll )
 	{
 		Debug.Log ("onColliderEnter");
@@ -32,31 +32,26 @@ public class Axe : MonoBehaviour {
 			{
 				hitCollider = ll;
 				Debug.Log ("Find Enemy");
-				axe_animation.setHit(true);
-//				axe_animation.goAttack();
+				hero_animation.setHit(true);
+//				hero_animation.goAttack();
 				state++;
 			}
 		}
 	}
-
+	
 	public void onColliderExit( Collision ll )
 	{
 		if (ll.transform.tag.CompareTo ("Enemy") == 0 && hitCollider == ll ) {
-			axe_animation.setHit(false);		
+			hero_animation.setHit(false);		
 		}
 	}
-
+	
 	public void onSkillClicked()
 	{
-//		anim.SetInteger ("Skill" , 3 );
-		axe_animation.doSkill (3);
+		//		anim.SetInteger ("Skill" , 3 );
+		hero_animation.doSkill (3);
 	}
-
-	public void onAttackFinished()
-	{
-//		state++;
-	}
-
+	
 	public IEnumerator PlayerState()
 	{
 		bool isOut = true;
@@ -67,17 +62,17 @@ public class Axe : MonoBehaviour {
 			case 0:
 				yield return new WaitForSeconds( 5 );
 				state++;
-				axe_animation.moveForward();
+				hero_animation.moveForward();
 				break;
 				//running
 			case 1:
-//				yield return new WaitForSeconds( 1 );
+				//				yield return new WaitForSeconds( 1 );
 				yield return new WaitForEndOfFrame();
 				break;
 				//attack
 			case 2:
-				axe_animation.goAttack();
-//				state++;
+				hero_animation.goAttack();
+				//				state++;
 				yield return new WaitForEndOfFrame( );
 				break;
 			case 3:
@@ -87,7 +82,7 @@ public class Axe : MonoBehaviour {
 				break;
 			case 5:
 				yield return new WaitForSeconds( 10 );
-				axe_animation.goDead();
+				hero_animation.goDead();
 				isOut = false;
 				break;
 			}
